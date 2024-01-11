3c3dbc6ecbc8f3f58ea843cc3b41ca3b635537f3
skaimarvas <skaimarvas@gmail.com>
2024-01-11 14:34:26 +0300

    Tüm görevler tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("*********************Point******************************");
         Point first = new Point(6,5);
         Point second = new Point(3,1);
 
@@ -12,6 +12,17 @@ public class Main {
         Point point = new Point(0,0);
         System.out.println("Point distance(0,0): "+ point.distance());
 
-
+        System.out.println("************************Player********************");
+        Player player1 = new Player("Dante",200, Player.Weapon.SWORD);
+        System.out.println(player1);
+        System.out.println("Health Remaining: " + player1.healthRemaining() );
+        player1.loseHealth(36);
+        System.out.println("After Damage: " + player1.healthRemaining() );
+        player1.restoreHealth(25);
+        System.out.println("After Heal: " + player1.healthRemaining());
+        player1.loseHealth(200);
+        System.out.println("After Damage: " + player1.healthRemaining() );
+        player1.restoreHealth(300);
+        System.out.println("After Damage: " + player1.healthRemaining() );
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,42 @@
 public class Player {
+    private String name;
+    private int healthPercentage;
+
+    private Weapon weapon;
+
+    public enum Weapon {
+        AK47,
+        SWORD,
+        WAND,
+    }
+
+    public Player(String name, int healthPercentage, Weapon weapon) {
+        this.name = name;
+        this.healthPercentage = Math.max(0,Math.min(healthPercentage, 100));
+        this.weapon = weapon;
+    }
+   public int healthRemaining(){
+        return healthPercentage;
+   }
+   public void loseHealth(int damage){
+        healthPercentage = Math.max(0, healthPercentage - damage);
+
+        if(healthPercentage <= 0){
+            System.out.println(name + " player has been knocked out of game");
+        }
+   }
+
+   public void restoreHealth(int healthPotion){
+        healthPercentage = Math.min(100, healthPercentage + healthPotion);
+
+   }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", healthPercentage=" + healthPercentage +
+                ", weapon=" + weapon +
+                '}';
+    }
 }
